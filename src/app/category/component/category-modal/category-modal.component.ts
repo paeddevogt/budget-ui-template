0adc7bb08eae14fac66f9f5149fb8fd556ba0e2e
Patrick Vogt <paedde.vogt@gmail.com>
2024-11-22 17:29:29 +0100

    Kategorien erstellen

--- src/app/category/component/category-modal/category-modal.component.ts
+++ src/app/category/component/category-modal/category-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import {
   IonButton,
   IonButtons,
@@ -11,11 +11,17 @@ import {
   IonItem,
   IonTitle,
   IonToolbar,
-  ModalController
+  ModalController,
+  ViewDidEnter
 } from '@ionic/angular/standalone';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { close, save, text, trash } from 'ionicons/icons';
+import { CategoryService } from '../../service/category.service';
+import { LoadingIndicatorService } from '../../../shared/service/loading-indicator.service';
+import { ToastService } from '../../../shared/service/toast.service';
+import { finalize } from 'rxjs';
+import { CategoryUpsertDto } from '../../../shared/domain';
 
 @Component({
   selector: 'app-category-modal',
@@ -38,10 +44,21 @@ import { close, save, text, trash } from 'ionicons/icons';
     IonFabButton
   ]
 })
-export default class CategoryModalComponent {
+export default class CategoryModalComponent implements ViewDidEnter {
   // DI
+  private readonly categoryService = inject(CategoryService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly loadingIndicatorService = inject(LoadingIndicatorService);
   private readonly modalCtrl = inject(ModalController);
+  private readonly toastService = inject(ToastService);
 
+  @ViewChild('nameInput') nameInput?: IonInput;
+
+  ionViewDidEnter(): void {
+    this.nameInput?.setFocus();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
   constructor() {
     // Add all used Ionic icons
     addIcons({ close, save, text, trash });
@@ -52,10 +69,28 @@ export default class CategoryModalComponent {
   }
 
   save(): void {
-    this.modalCtrl.dismiss(null, 'save');
+    this.loadingIndicatorService.showLoadingIndicator({ message: 'Saving category' }).subscribe(loadingIndicator => {
+      const category = this.categoryForm.value as CategoryUpsertDto;
+      this.categoryService
+        .upsertCategory(category)
+        .pipe(finalize(() => loadingIndicator.dismiss()))
+        .subscribe({
+          next: () => {
+            this.toastService.displaySuccessToast('Category saved');
+            this.modalCtrl.dismiss(null, 'refresh');
+          },
+          error: error => this.toastService.displayWarningToast('Could not save category', error)
+        });
+    });
   }
 
   delete(): void {
     this.modalCtrl.dismiss(null, 'delete');
   }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  readonly categoryForm = this.formBuilder.group({
+    id: [null! as string], // hidden
+    name: ['', [Validators.required, Validators.maxLength(40)]]
+  });
 }
---
+++ src/app/shared/service/loading-indicator.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular/standalone';
+import { LoadingOptions } from '@ionic/core/dist/types/components/loading/loading-interface';
+import { from, Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class LoadingIndicatorService {
+  private readonly loadingController = inject(LoadingController);
+
+  showLoadingIndicator = (options: LoadingOptions): Observable<HTMLIonLoadingElement> =>
+    from(
+      this.loadingController.create(options).then(loading => {
+        loading.present();
+        return loading;
+      })
+    );
+}
